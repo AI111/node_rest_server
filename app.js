190a7c44c0eea39d3069b3f04053ecdc6e467fbe
AN225 <sashaandreev7@gmail.com>
2015-06-08 19:42:00 +0300

    add Geo point CRUD

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var guides = require('./routes/guides');
+var points = require('./routes/points');
 var app = express();
 
 // view engine setup
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/guides',guides);
+app.use('/points',points);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
